6c7c3d3995b7564edaec964efc1a6240bde58fce
Laboratorio UNAM-Apple <laboratoriounam-apple@macbook-pro-de-laboratorio-5.local>
2019-03-06 13:59:26 -0600

    update: button action

--- problemGenerator/ViewController.swift
+++ problemGenerator/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    @IBAction func boton1(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

baae0195c18ff42b4f5e15bc1f3e454acfd804ec
Laboratorio UNAM-Apple <laboratoriounam-apple@macbook-pro-de-laboratorio-5.local>
2019-03-06 13:45:58 -0600

    Initial Commit

---
+++ problemGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  problemGenerator
+//
+//  Created by Laboratorio UNAM-Apple on 06/03/19.
+//  Copyright © 2019 Laboratorio UNAM-Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

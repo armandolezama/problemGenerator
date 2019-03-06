b9cdfc59cf78ed663da6a5aa181dd8c2dd9d4bf2
Laboratorio UNAM-Apple <laboratoriounam-apple@macbook-pro-de-laboratorio-5.local>
2019-03-06 14:57:02 -0600

    update: se ha creado una vista programática

--- problemGenerator/ViewController.swift
+++ problemGenerator/ViewController.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 
-
-
 class ViewController: UIViewController {
 
     @IBAction func boton1(_ sender: UIButton) {
@@ -18,9 +16,29 @@ class ViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let viewRoja = UIView()
+        
+        viewRoja.backgroundColor = .red
+        
+        view.addSubview(viewRoja)
+        
+        viewRoja.translatesAutoresizingMaskIntoConstraints = false
+        
+        topAnchor.constraint(equalTo: top).isActive = true
+        leadingAnchor.constraint(equalTo: leading).isActive = true
+        trailingAnchor.constraint(equalTo: trailing).isActive = true
+        bottomAnchor.constraint(equalTo: bottom).isActive = true
+
     }
 
 
+}
+
+extension UIView {
+    func anchor (top: NSLayoutYAxisAnchor, leading: NSLayoutXAxisAnchor, trailing: NSLayoutXAxisAnchor, bottom: NSLayoutYAxisAnchor) {
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
 }
 
